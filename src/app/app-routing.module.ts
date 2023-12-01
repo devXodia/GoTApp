fb03a33d580d7760ba74517a19ae8240b5c4a742
Alen Alduk <youstylez@gmail.com>
2023-12-01 21:04:33 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { HousesComponent } from './components/houses/houses.component';
+import { PersonsComponent } from './components/persons/persons.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavComponent },
+  { path: 'houses', component: HousesComponent },
+  { path: 'persons', component: PersonsComponent },
+  { path: 'quotes', component: QuotesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HousesComponent } from './components/houses/houses.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PersonsComponent } from './components/persons/persons.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HousesComponent,
+    NavComponent,
+    PersonsComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
